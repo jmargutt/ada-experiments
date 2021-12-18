a538c6bff506c8aadaff8e086962c066bffe3388
jmargutti <jacopo.margutti@gmail.com>
2021-12-18 17:02:42 +0100

    test

--- wealth-index/csv-to-boxes.py
+++ wealth-index/csv-to-boxes.py
@@ -1,21 +1,16 @@
 import pandas as pd
-import numpy as np
-import math
-import rasterio
-from rasterio.transform import from_origin
 import os
-from tqdm import tqdm
-from scipy.interpolate import griddata
+import click
+import mercantile
 import geopandas as gpd
 from shapely.geometry import box
+from pyquadkey2 import quadkey
 
 
-def meters_to_longitude(meters, latitude):
-    """ convert meters to decimal degrees, based on latitude
-    World circumference measured around the equator is 40,075.017 km (source: wikipedia)
-    40075.017 / 360 = 111.319491667 km/deg
-    """
-    return meters / (111.319491667 * 1000 * math.cos(latitude * (math.pi / 180)))
+def calculate_tile_bbox(row):
+    qk = str(quadkey.from_geo((row['latitude'], row['longitude']), 14))
+    b = mercantile.bounds(mercantile.quadkey_to_tile(qk))
+    return box(b.west, b.south, b.east, b.north)
 
 
 def meters_to_latitude(meters):
@@ -26,33 +21,32 @@ def meters_to_latitude(meters):
     return meters / (111.132952778 * 1000)
 
 
-def csv_to_raster(input_csv, output_raster, resolution=2400):
+def csv_to_raster(input_csv, output_vector):
     """ convert csv file to raster """
 
     # read csv
     df = pd.read_csv(input_csv)
 
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
-    gdf.to_file(output_raster.replace('.tif', '._points.gpkg'), driver='GPKG')
-
-    for ix, row in gdf.iterrows():
-        lon_size = meters_to_longitude(resolution, row['latitude'])
-        lat_size = meters_to_latitude(resolution)
-        gdf.at[ix, 'geometry'] = box(row['longitude']-lon_size*0.5, row['latitude']-lat_size*0.5,
-                                     row['longitude']+lon_size*0.5, row['latitude']+lat_size*0.5)
-    gdf.to_file(output_raster.replace('.tif', '._squares.gpkg'), driver='GPKG')
+    gdf['geometry'] = gdf.apply(calculate_tile_bbox, axis=0)
+    gdf.to_file(output_vector, driver='GPKG')
 
 
-if __name__ == "__main__":
-
-    input_dir = "relative-wealth-index-april-2021"
-    output_dir = input_dir+"-geotiff"
+@click.command()
+@click.option('--data', default='data-input', help='input directory')
+@click.option('--dest', default='data-output', help='output directory')
+def convert_from_dir(data, dest):
+    input_dir = data
+    output_dir = dest
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    dist_all = pd.DataFrame()
     for file in os.listdir(input_dir):
         print(f"processing {file}")
         input_filepath = os.path.join(input_dir, file)
-        output_filepath = os.path.join(output_dir, file.replace('.csv', '.tif'))
+        output_filepath = os.path.join(output_dir, file.replace('.csv', '.gpkg'))
         csv_to_raster(input_filepath, output_filepath)
+
+
+if __name__ == "__main__":
+    convert_from_dir()
